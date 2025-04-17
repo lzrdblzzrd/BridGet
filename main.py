11aca17cfd064a58c71c452cda36f7441c634d37
lzrdblzzrd <lzrdblzzrd@gmail.com>
2025-04-17 13:57:25 +0300

    Updated

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
+import os
+import pathlib
+import shutil
+import tarfile
 import urllib.request
+
+TOR_URL = "https://mirror.oldsql.cc/tor/dist/torbrowser/14.5/tor-expert-bundle-windows-x86_64-14.5.tar.gz"
+BRIDGES_URL = "https://torscan-ru.ntc.party/relays.txt"
+TOR_FILE = pathlib.Path("tor.exe")
+
+
+# TORRC_FILE = pathlib.Path("torrc")
 
 
 def main():
+    if not TOR_FILE.is_file():
+        download_tor()
+    # if not TORRC_FILE.is_file():
+    #     create_torrc()
+    download_tor_bridges()
+
+
+def download_tor():
     try:
-        with urllib.request.urlopen(url="https://torscan-ru.ntc.party/relays.txt") as response:
+        urllib.request.urlretrieve(url=TOR_URL, filename="tor.tar.gz")
+        with tarfile.open(name="tor.tar.gz") as tar:
+            tar.extractall()
+        shutil.move(src="tor/tor.exe", dst="tor.exe")
+        shutil.move(src="data/geoip", dst="geoip")
+        shutil.move(src="data/geoip6", dst="geoip6")
+        for directory in ["data", "docs", "tor"]:
+            shutil.rmtree(path=directory)
+        os.remove(path="tor.tar.gz")
+
+    except Exception as exception:
+        print(exception)
+
+
+# def create_torrc():
+#     ...
+
+
+def download_tor_bridges():
+    try:
+        with urllib.request.urlopen(url=BRIDGES_URL) as response:
             bridges = response.read().decode(encoding="UTF-8").strip().split(sep="\n")
             with open(file="bridges.conf", mode="w") as file:
                 for bridge in bridges:
